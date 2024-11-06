7b98bdbc657c3dc2d102093fe42f2424e0e68074
Francis Ricle <francisricleruiz@gmail.com>
2024-11-06 06:09:13 -0300

    entrega

--- app/src/layouts/app.tsx
+++ app/src/layouts/app.tsx
@@ -9,6 +9,28 @@ export function ErrorBoundary() {
     <Component />
   </div>;
 }
+const SignOut = ({ onClick }: { onClick: any }) => {
+  const styles = [
+    "inline-block",
+    "shrink-0",
+    "rounded-md",
+    "border",
+    "border-blue-600",
+    "bg-blue-600",
+    "px-12",
+    "py-3",
+    "text-sm",
+    "font-medium",
+    "text-white",
+    "transition",
+    "hover:bg-transparent",
+    "hover:text-blue-600",
+    "focus:outline-none",
+    "focus:ring",
+    "active:text-blue-500",
+  ].reduce((prev: string, curr: string) => prev.concat(' ', curr))
+  return <Button onClick={onClick} className={`${styles}`}>Sign Out</Button>
+}
 export function Component() {
   const token = window.localStorage.getItem("token");
   if (!token) {
@@ -22,7 +44,7 @@ export function Component() {
       });
   }
   return <div>
-    <Button onClick={onClick}>Sign Out</Button>
+    <SignOut onClick={onClick} />
     <Outlet />
   </div>
 }
